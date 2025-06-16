21e4b31f5a3468fc9f7479e88eeb3ebbb5131769
KHATRIRAMESH <khatriramesh972@gmail.com>
2025-06-16 21:44:35 +0545

    update profile logic changed

--- src/controllers/cutomer.controller.js
+++ src/controllers/cutomer.controller.js
@@ -3,12 +3,12 @@ import { db } from "../database/connection/dbConnection.js";
 import { customers } from "../database/schema/customer-schema.js";
 
 export async function updateCustomerProfile(req, res) {
-  if (!req.isAuthenticated()) {
-    return res.status(401).json({
-      success: false,
-      message: "You must be logged in to update your profile.",
-    });
-  }
+  //   if (!req.isAuthenticated()) {
+  //     return res.status(401).json({
+  //       success: false,
+  //       message: "You must be logged in to update your profile.",
+  //     });
+  //   }
 
   try {
     const { email, password } = req.body;
@@ -19,10 +19,14 @@ export async function updateCustomerProfile(req, res) {
         message: "Email and password are required.",
       });
     }
-    await db
-      .update(customers)
-      .set({ password: password })
-      .where(eq(customers.email, email));
+    // await db
+    //   .update(customers)
+    //   .set({ password: password })
+    //   .where(eq(customers.email, email));
+    await db.insert(customers).values({
+      email: email,
+      password: password,
+    });
     return res.status(200).json({
       success: true,
       message: "Password updated successfully.",
